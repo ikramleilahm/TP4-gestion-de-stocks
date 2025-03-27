3d4e661ba08394c80cdd930fa4cb2d442123a494
ikramleilahm <leilaikram94@gmail.com>
2025-03-27 11:16:04 +0100

    first commit

---
+++ src/main/java/com/example/demo/GestionDeStocksApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDeStocksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDeStocksApplication.class, args);
+	}
+
+}
